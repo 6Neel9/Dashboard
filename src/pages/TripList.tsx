640dc56971eafa69106a8f739bcdeec632348c8e
JeminButani <jdbutani2002@gmail.com>
2023-06-28 11:28:42 +0530

    Triplist updated

--- src/pages/TripList.tsx
+++ src/pages/TripList.tsx
@@ -30,48 +30,48 @@ import "../Styles.css"
 const TripList = ({ data }: any) => {
     const navigate = useNavigate();
 
-    type DriverDataType = {
-        did: Number,
-        fname: String,
-        lname: String,
-        bdate: Date,
-        dlno: String,
-        working: Boolean,
-        trips: [
-            {
-                tid: Number,
-                stime: Date,
-                etime: Date,
-                sloc: [],
-                eloc: [],
-                revenue: Number,
-                city: {
-                    id: String,
-                    name: String,
-                    state: String,
-                },
-            },
-        ],
+    // type DriverDataType = {
+    //     did: Number,
+    //     fname: String,
+    //     lname: String,
+    //     bdate: Date,
+    //     dlno: String,
+    //     working: Boolean,
+    //     trips: [
+    //         {
+    //             tid: Number,
+    //             stime: Date,
+    //             etime: Date,
+    //             sloc: [],
+    //             eloc: [],
+    //             revenue: Number,
+    //             city: {
+    //                 id: String,
+    //                 name: String,
+    //                 state: String,
+    //             },
+    //         },
+    //     ],
 
-    };
-    type TripDataType =
-        {
-            tid?: Number,
-            stime?: Date,
-            etime?: Date,
-            sloc?: [],
-            eloc?: [],
-            revenue?: Number,
-            city?: {
-                id: String,
-                name: String,
-                state: String,
-            },
+    // };
+    // type TripDataType =
+    //     {
+    //         tid?: Number,
+    //         stime?: Date,
+    //         etime?: Date,
+    //         sloc?: [],
+    //         eloc?: [],
+    //         revenue?: Number,
+    //         city?: {
+    //             id: String,
+    //             name: String,
+    //             state: String,
+    //         },
 
-        }[];
+    //     }[];
 
     const toolbarOptions: ToolbarItems[] = ['Search', 'ExcelExport'];
-    const [tripData, setTripData] = useState<TripDataType>([]);
+    const [tripData, setTripData] = useState<any[]>([]);
     const pageSettings: PageSettingsModel = { pageSize: 10, pageCount: 5 }
     const filterOptions: FilterSettingsModel = { type: "Menu" }
 
@@ -85,15 +85,47 @@ const TripList = ({ data }: any) => {
         }
     };
 
-    function numberFormat(x: string) {
-        x = x.toString();
-        var lastThree = x.substring(x.length - 3);
-        var otherNumbers = x.substring(0, x.length - 3);
-        if (otherNumbers !== "") lastThree = "," + lastThree;
-        var res: string = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
-        return res;
-    }
+    // function numberFormat(x: string) {
+    //     x = x.toString();
+    //     var lastThree = x.substring(x.length - 3);
+    //     var otherNumbers = x.substring(0, x.length - 3);
+    //     if (otherNumbers !== "") lastThree = "," + lastThree;
+    //     var res: string = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+    //     return res;
+    // }
 
+
+    
+        // useEffect(() => {
+        //     var tr_data: object[] = [];
+        //     var srno: number = 1;
+        //     data.forEach((e: DriverDataType) => {
+        //         const temp_trips = e.trips;
+        //         temp_trips.forEach((ele)=>{
+        //             const city = ele.city;
+        //             const temp: {srno:Number, tid: Number, stime: String, etime: String, sloc: [], eloc: [], revenue: String, city: String}={
+        //                 srno:srno, tid: ele.tid, stime: String(ele.stime).slice(0, 10), etime: String(ele.etime).slice(0, 10), sloc: ele.sloc, eloc: ele.eloc, revenue: "₹ " + numberFormat(String((ele.revenue))), city: city['name']
+        //             }
+        //             tr_data.push(temp)
+        //             srno += 1;
+        //         })
+                
+        //     });
+        //     setTripData(tr_data);
+        // }, [data]);
+
+
+        
+        useEffect(() => {
+          fetch("http://localhost:5000/yuja-sm/v1/trips", {
+            method: "GET",
+          })
+            .then((res) => res.json())
+            .then((i) => {
+              setTripData(i);
+              console.log(i);
+            })
+        }, []);
     const rowSelected = () => {
         if (grid) {
             // const selectedrowindex: number[] = grid.getSelectedRowIndexes();
@@ -116,24 +148,6 @@ const TripList = ({ data }: any) => {
         format: string;
         textAlign: string;
     }
-
-    useEffect(() => {
-        var tr_data: object[] = [];
-        var srno: number = 1;
-        data.forEach((e: DriverDataType) => {
-            const temp_trips = e.trips;
-            temp_trips.forEach((ele)=>{
-                const city = ele.city;
-                const temp: {srno:Number, tid: Number, stime: String, etime: String, sloc: [], eloc: [], revenue: String, city: String}={
-                    srno:srno, tid: ele.tid, stime: String(ele.stime).slice(0, 10), etime: String(ele.etime).slice(0, 10), sloc: ele.sloc, eloc: ele.eloc, revenue: "₹ " + numberFormat(String((ele.revenue))), city: city['name']
-                }
-                tr_data.push(temp)
-                srno += 1;
-            })
-            
-        });
-        setTripData(tr_data);
-    }, [data]);
 
     return (
         <div className="largeMargin marginTopLarge largePadding mainBackground rounded3XLarge mainBorder mainShadow">

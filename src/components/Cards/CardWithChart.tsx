ac318a56b5a61addecd17991729f560da3f04339
JeminButani <jdbutani2002@gmail.com>
2023-06-28 10:26:04 +0530

    CardWithChart updated

--- src/components/Cards/CardWithChart.tsx
+++ src/components/Cards/CardWithChart.tsx
@@ -4,7 +4,8 @@ import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 
 const CardWithChart = ({ prop1, prop2, chart }: any) => {
     return (
-        <div className=" heightFitContent   textLeft flexJustifyBetween displayGrid gridCols3 divideXSmall widthFull mainShadow ">
+        <div className='displayFlex textLeft heightFitContent smallMargin  mediumPadding marginBottomMedium mainBackground mainBorder  roundedExtraXLarge'>
+        <div className=" heightFitContent   textLeft flexJustifyBetween displayGrid gridCols3 divideXSmall widthFull ">
             <div className="marginRightMedium">
                 <div className='displayFlex flexJustifyBetween flexCol'>
                     <div className="displayFlex flexJustifyBetween ">
@@ -39,6 +40,7 @@ const CardWithChart = ({ prop1, prop2, chart }: any) => {
             </div>
 
 
+        </div>
         </div>
     )
 }

e84536e993e7186ddfeafa82cd11139c8c386dbb
6neel9 <nilmandal300@gmail.com>
2023-07-01 10:02:08 +0530

    parking location updated

--- src/pages/Parking.tsx
+++ src/pages/Parking.tsx
@@ -11,32 +11,62 @@ import customMarkerIcon from '../components/Map/images/dot-inside-a-circle.png';
 
 const locations = [
     {
-      name: 'PDEU',
-      latlng: [23.1566, 72.6654],
+      name: 'detected-parking-1',
+      latlng: [23.1500, 72.6600],
       driver: "Sarang",
       autoNo: "GJ 18 1234"
     },
     {
-      name: 'DAIICT',
-      latlng: [23.1885 , 72.6289],
+      name: 'detected-parking-2',
+      latlng: [23.1861 , 72.6293],
       driver: "Jemin",
       autoNo: "GJ 18 5678"
     },
     {
-      name: 'GNLU',
-      latlng: [23.1537 , 72.6608],
+      name: 'detected-parking-3',
+      latlng: [23.1435 , 72.6510],
       driver: "Nilanjan",
       autoNo: "GJ 18 1357"
     },
     {
-      name: 'GIFT CITY',
-      latlng: [23.1586, 72.6831],
+      name: 'detected-parking-4',
+      latlng: [23.1585, 72.6828],
       driver: "Sohan",
       autoNo: "GJ 18 2468"
     },
     {
-      name: 'Gandhinagar',
-      latlng: [23.237560 , 72.647781],
+      name: 'detected-parking-5',
+      latlng: [23.2372 , 72.6488],
+      driver: "Ruchit",
+      autoNo: "GJ 18 8907"
+    },
+    {
+      name: 'detected-parking-6',
+      latlng: [23.2375 , 72.6378],
+      driver: "Ruchit",
+      autoNo: "GJ 18 8907"
+    },
+    {
+      name: 'detected-parking-7',
+      latlng: [23.2072 , 72.6388],
+      driver: "Ruchit",
+      autoNo: "GJ 18 8907"
+    },
+    {
+      name: 'detected-parking-8',
+      latlng: [23.2140 , 72.6888],
+      driver: "Ruchit",
+      autoNo: "GJ 18 8907"
+    },
+    {
+      name: 'detected-parking-9',
+      latlng: [23.1915 , 72.6488],
+      driver: "Ruchit",
+      autoNo: "GJ 18 8907"
+    },
+    {
+      name: 'detected-parking-10',
+      latlng: [23.1872 , 72.5588],
       driver: "Ruchit",
       autoNo: "GJ 18 8907"
     }

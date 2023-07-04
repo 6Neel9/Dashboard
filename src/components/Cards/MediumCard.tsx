c43c118bad87db783ea03f1ed5382d3b650e9f7a
JeminButani <jdbutani2002@gmail.com>
2023-07-04 11:13:53 +0530

    MediumCard css classes updated

--- src/components/Cards/MediumCard.tsx
+++ src/components/Cards/MediumCard.tsx
@@ -5,20 +5,20 @@ import "../../Styles.scss"
 
 const MediumCard = ({ props }: any) => {
   return (
-    <div className="container mediumContainer smallMargin mediumPadding mainShadow">
-      <h1 className=" textStyle normalText smallPadding extraSmallText">{props.title.toUpperCase()}</h1>
-      <div className="textStyle smallPadding ">
+    <div className="mediumCardContainer">
+      <h1 className="mediumCardTitleText">{props.title.toUpperCase()}</h1>
+      <div className="mediumCardDurationText">
         {props.duration.toLowerCase()}
       </div>
-      <div className="largeText textStyle paddingTopSmall mediumWeightText extraBoldWeightText">
+      <div className="mediumCardValueText">
         {props.value.toUpperCase()}
       </div>
-      {props.icon && <div >
-
-        {props.icon === "positive" ?
-          <h1 className="percent paddingTopSmall percentIncrease"><SlArrowUp className="percentIcon" /> {props.percent}%</h1> :
-          <h1 className="percent paddingTopSmall percentDecrease"><SlArrowDown className="percentIcon" /> {props.percent}%</h1>
+      {props.percent && <div>
+        {Number(props.percent) === 0 ? <h1 className="mediumCardNeutral">0%</h1> : props.percent > 0 ?
+          <h1 className="mediumCardPositive"><SlArrowUp className="percentIcon" /> {props.percent}%</h1> :
+          <h1 className="mediumCardNegative"><SlArrowDown className="percentIcon" /> {props.percent}%</h1>
         }
+
       </div>}
 
     </div>
